6e13e322a90927c3b5a092024896326cc49da5d8
Kevin Winata <kevin.winata170600@gmail.com>
2024-04-01 14:33:54 +0700

    adding module and application, deleting TwitchRetrofitInstance

--- app/src/main/java/com/example/internproject/dagger/TwitchApiModule.kt
+++ app/src/main/java/com/example/internproject/dagger/TwitchApiModule.kt
@@ -1,13 +1,23 @@
-package com.example.internproject.network
+package com.example.internproject.dagger
 
+import com.example.internproject.network.TwitchService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
-class TwitchRetrofitInstance {
+@Module
+@InstallIn(SingletonComponent::class)
+object TwitchApiModule {
 
-    companion object {
+    @Provides
+    @Singleton
+    fun provideApiService() : TwitchService {
         val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val okHttpClient = OkHttpClient.Builder().addInterceptor(interceptor).build()
 
@@ -20,6 +30,8 @@ class TwitchRetrofitInstance {
         fun apiService() : TwitchService {
             return  retrofit.create(TwitchService::class.java)
         }
+
+        return apiService()
     }
 
 }
---
+++ app/src/main/java/com/example/internproject/dagger/TwitchApplication.kt
@@ -0,0 +1,8 @@
+package com.example.internproject.dagger
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TwitchApplication : Application() {
+}
