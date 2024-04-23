0b3a803aba0d76475c1e42f4b0ae0931ab93434f
Kevin Winata <kevin.winata170600@gmail.com>
2024-04-23 11:32:57 +0700

    add const to constant variable

--- app/src/main/java/com/example/internproject/util/TwitchConstants.kt
+++ app/src/main/java/com/example/internproject/util/TwitchConstants.kt
@@ -4,14 +4,14 @@ import android.net.Uri
 import com.example.internproject.BuildConfig
 
 object TwitchConstants {
-    val CLIENT_ID = BuildConfig.CLIENT_ID
-    val DUMMY_USER_ID = "654851614"
-    val TOKEN_ARGUMENT = "twitch_access_token"
-    val IMAGE_WIDTH = 1280
-    val IMAGE_HEIGHT = 960
-    val VIDEO_USER_ID = "70225218"
-    val VIDEO_TYPE = "archive"
-    val REDIRECT_URI = "https://com.example.internproject"
+    const val CLIENT_ID = BuildConfig.CLIENT_ID
+    const val DUMMY_USER_ID = "654851614"
+    const val TOKEN_ARGUMENT = "twitch_access_token"
+    const val IMAGE_WIDTH = 1280
+    const val IMAGE_HEIGHT = 960
+    const val VIDEO_USER_ID = "70225218"
+    const val VIDEO_TYPE = "archive"
+    const val REDIRECT_URI = "https://com.example.internproject"
     val TWITCH_AUTH_URL = String.format("https://id.twitch.tv/oauth2/authorize?client_id=%s&redirect_uri=%s&response_type=token&scope=user:read:follows", CLIENT_ID, REDIRECT_URI)
     fun extractAccessToken(uri: Uri?): String? {
         return uri?.fragment?.split("&")?.map { it.split("=") }
